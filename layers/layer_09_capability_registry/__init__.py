2871f9411c76cb89c8ce10ce6224568b7f793642
Muhammad Nauman Tahir <muhammadnauman.tahir0@gmail.com>
2026-09-01 18:43:32 +0500

    Phase 4: expose capability registry API

--- layers/layer_09_capability_registry/__init__.py
+++ layers/layer_09_capability_registry/__init__.py
@@ -1,1 +1,5 @@
 """Layer 09: Capability Registry."""
+
+from .registry import CapabilityRecord, CapabilityRegistry, RegistryError
+
+__all__ = ["CapabilityRecord", "CapabilityRegistry", "RegistryError"]
