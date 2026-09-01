08ca6147ba128bf8103c65625a3c457d470846b4
Muhammad Nauman Tahir <muhammadnauman.tahir0@gmail.com>
2026-09-01 18:37:44 +0500

    feat(layer-8): export evolution engine

--- layers/layer_08_evolution/__init__.py
+++ layers/layer_08_evolution/__init__.py
@@ -1,1 +1,17 @@
 """Layer 08: Evolution."""
+
+from .evolution_engine import (
+    CapabilityPlan,
+    EvolutionEngine,
+    EvolutionError,
+    GeneratedCapability,
+    TestResults,
+)
+
+__all__ = [
+    "CapabilityPlan",
+    "EvolutionEngine",
+    "EvolutionError",
+    "GeneratedCapability",
+    "TestResults",
+]
