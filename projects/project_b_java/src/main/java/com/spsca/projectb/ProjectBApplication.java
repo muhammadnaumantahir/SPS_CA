baf92c4c3763e082390b7e46c2e783c189c19b02
Muhammad Nauman Tahir <muhammadnauman.tahir0@gmail.com>
2026-09-02 10:17:46 +0500

    PHASE8: add Java Spring Boot entrypoint

---
+++ projects/project_b_java/src/main/java/com/spsca/projectb/ProjectBApplication.java
@@ -0,0 +1,9 @@
+package com.spsca.projectb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBApplication {
+    public static void main(String[] args) { SpringApplication.run(ProjectBApplication.class, args); }
+}
