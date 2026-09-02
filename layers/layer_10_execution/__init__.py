15dcb6ae8a07a22f87f4088bcfe87b95664b5255
Muhammad Nauman Tahir <muhammadnauman.tahir0@gmail.com>
2026-09-02 10:00:34 +0500

    PHASE5-S1.4: expose Layer 10 execution public API

--- layers/layer_10_execution/__init__.py
+++ layers/layer_10_execution/__init__.py
@@ -1,1 +1,26 @@
-"""Layer 10: Execution."""
+"""Layer 10: Execution public API."""
+
+from .execution_engine import ExecutionEngine, ExecutionEngineError
+from .models import (
+    Change,
+    ExecutionRecord,
+    ExecutionResult,
+    ExecutionStatus,
+    FileEdit,
+    FileSnapshot,
+    RollbackResult,
+    TestOutcome,
+)
+
+__all__ = [
+    "Change",
+    "ExecutionEngine",
+    "ExecutionEngineError",
+    "ExecutionRecord",
+    "ExecutionResult",
+    "ExecutionStatus",
+    "FileEdit",
+    "FileSnapshot",
+    "RollbackResult",
+    "TestOutcome",
+]
