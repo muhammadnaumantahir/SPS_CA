6d7dd399058722723ba58d8959099f228aaeb42f
Muhammad Nauman Tahir <muhammadnauman.tahir0@gmail.com>
2026-09-03 14:58:40 +0500

    Export self-improvement benchmark

---
+++ evaluation/__init__.py
@@ -0,0 +1,5 @@
+"""Deterministic evaluation harnesses for SPS-CA."""
+
+from .self_improvement_benchmark import SelfImprovementBenchmark, SelfImprovementBenchmarkResult
+
+__all__ = ["SelfImprovementBenchmark", "SelfImprovementBenchmarkResult"]
