cd02319d197d3fd280095d87f4b537d1365523cf
Muhammad Nauman Tahir <muhammadnauman.tahir0@gmail.com>
2026-09-04 11:01:04 +0500

    test: add concrete Layer 5 learning compatibility module

---
+++ layers/layer_05_experience/long_term_learning.py
@@ -0,0 +1,5 @@
+"""Legacy import wrapper for the canonical Layer 5 long-term learning store."""
+
+from layers.layer_05_experience_core.long_term_learning import *
+
+__all__ = ["DEFAULT_LONG_TERM_PATH", "LongTermLearningStore"]
